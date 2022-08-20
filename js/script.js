bb5b0c3698f45417647d0fed4e9ecd1f1b725a42
manish mandal <76776194mm@gmail.com>
2022-08-20 13:11:21 +0530

    update js

--- js/script.js
+++ js/script.js
@@ -2,34 +2,65 @@
 
 const Fname=document.querySelector('#fname');
 const Lname=document.querySelector('#lname');
-const email=document.querySelector('#fname');
-const pswrd=document.querySelector('#fname');
-const submitBtn=document.querySelector('.main__form-btn');
+const email=document.querySelector('#email');
+const pswrd=document.querySelector('#pswd');
+const submitBtn=document.querySelector('.button');
+const icon=document.querySelector(`.Fname-icon`);
+const pattern=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-function errorFunc(obj){
-    obj.classList.remove('hidden');
-}
 
 submitBtn.addEventListener('click',function(){
-    console.log(2);
 
-    var firstName=Fname.value.trim();
-    var lastName=Lname.value.trim();
-    var emailValue=email.value.trim();
-    var passwordValue=pswrd.value.trim();
-
-
-    if(firstName.value===''){
-        errorFunc(Fname);
+    if(Fname.value===''){
+        const icon=document.querySelector(`.Fname-icon`);
+        icon.classList.remove('hidden');
+        document.querySelector('.Fname-msge').classList.remove('hidden');
+        Fname.placeholder="";
+        Fname.style.border="1px solid red";
     }
     else{
-        successFunc(Fname);
+       Fname.style.border="2px solid hsl(154, 59%, 51%)";
     }
 
-    if(firstName.value===''){
-        errorFunc(Fname);
+    if(Lname.value===''){
+        const icon=document.querySelector(`.lname-icon`);
+        icon.classList.remove('hidden');
+        document.querySelector('.lname-msge').classList.remove('hidden');
+        Lname.placeholder="";
+        Lname.style.border="1px solid red";
+
     }
     else{
-        successFunc(Fname);
+        Lname.style.border="2px solid hsl(154, 59%, 51%)";
+    }
+
+    if(email.value===''){
+        const icon=document.querySelector(`.email-icon`);
+        icon.classList.remove('hidden');
+        document.querySelector('.email-msge-2').classList.remove('hidden');
+        email.style.border="1px solid red";
+        email.placeholder="";
+        
+    }
+    else if(!email.value.match(pattern)){
+        email.value="";
+        email.placeholder="email@Example/com";
+        // email. placeholder.style.color="red";
+        document.querySelector('.email-msge').classList.remove('hidden');
+    }
+    else{
+        email.style.border="2px solid hsl(154, 59%, 51%)";
+    }
+
+    if(pswrd.value===''){
+        const icon=document.querySelector(`.pswrd-icon`);
+        icon.classList.remove('hidden');
+        document.querySelector('.pswrd-msge').classList.remove('hidden');
+        pswrd.placeholder="";
+        pswrd.style.border="1px solid red";
+
+    }
+    else{
+        pswrd.style.border="2px solid hsl(154, 59%, 51%)";
     }
 });
