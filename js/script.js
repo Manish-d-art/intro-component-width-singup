8e960828d965e215389299b11471a3be0e95d21c
manish mandal <76776194mm@gmail.com>
2022-08-20 10:42:27 +0530

    update js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
 "use strict"
+
+const Fname=document.querySelector('#fname');
+const Lname=document.querySelector('#lname');
+const email=document.querySelector('#fname');
+const pswrd=document.querySelector('#fname');
+const submitBtn=document.querySelector('.main__form-btn');
+
+function errorFunc(obj){
+    obj.classList.remove('hidden');
+}
+
+submitBtn.addEventListener('click',function(){
+    console.log(2);
+
+    var firstName=Fname.value.trim();
+    var lastName=Lname.value.trim();
+    var emailValue=email.value.trim();
+    var passwordValue=pswrd.value.trim();
+
+
+    if(firstName.value===''){
+        errorFunc(Fname);
+    }
+    else{
+        successFunc(Fname);
+    }
+
+    if(firstName.value===''){
+        errorFunc(Fname);
+    }
+    else{
+        successFunc(Fname);
+    }
+});
